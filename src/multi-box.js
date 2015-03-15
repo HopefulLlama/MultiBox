3230668ead7e8e9a401d7b993262d479f865649c
HopefulLlama <jonlaw10170@gmail.com>
2015-03-15 17:51:44 +0000

    - Implemented add button - Added minified version

--- src/multi-box.js
+++ src/multi-box.js
@@ -28,11 +28,14 @@ function MultiBox(name, cssClasses, items) {
 
 	this.generate = function() {
 		var _this = this;
+		
 		var container = $("#"+this.name);
 		container.empty();
+		
 		container.addClass("panel panel-default");
 		var panel = $('<div class="panel-body">');
 		panel.appendTo(container);
+		
 		var listOfItemsDiv = $('<div id="list-of-items-'+this.instance+'">');
 		for (var i = 0; i < this.items.length; i++) {
 			var item = $('<div class="col-xs-12">');
@@ -47,5 +50,30 @@ function MultiBox(name, cssClasses, items) {
 			});
 		}
 		listOfItemsDiv.appendTo(panel);
+
+		var footer = $('<div class="panel-footer">');
+		footer.appendTo(container);
+
+		var inputGroup = $('<div class="input-group">');
+		inputGroup.appendTo(footer);
+		var input = $('<input type="text" class="form-control">')
+		input.appendTo(inputGroup);
+		var inputGroupAddon = $('<span class="input-group-btn">');
+		inputGroupAddon.appendTo(inputGroup);
+		var addButton = $('<button type="button" class="btn btn-primary">+ Add</button>');
+		addButton.appendTo(inputGroupAddon);
+
+		var addItemToList = function() {
+			_this.addItem(input.val());
+			_this.generate();
+		};
+
+		addButton.click(addItemToList);
+		input.keydown(function(event) {
+			if(event.which == 13) {
+				event.preventDefault();
+				addItemToList();
+			}
+		});
 	};
 }
